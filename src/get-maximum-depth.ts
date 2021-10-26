fc873d3196dfbbc4ebe92fa5d37ef473f32dd200
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-10-26 03:04:35 -0300

    [feature] stack data structure implementation to provibe an idiomatic way to define an explicit stack within iterative versions of recursive functions

--- src/get-maximum-depth.ts
+++ src/get-maximum-depth.ts
@@ -3,12 +3,10 @@ import { isArray } from './is-array';
 export const getMaximumDepth = <TCollection extends unknown[]>(
   collection: TCollection,
 ) => {
-  let stack = [];
+  let stack = [...collection];
 
   for (let i = 0; i < collection.length; i++) {
     const element = collection[i];
-
-    let maxDepthUnknown = true;
 
     while (maxDepthUnknown) {
       if (isArray(element)) {
--- src/stack.ts
+++ src/stack.ts
@@ -1,5 +1,43 @@
 /* eslint-disable require-jsdoc */
-export class Stack {
-  first = null;
-  size = 0;
+
+interface IStack {
+  top: unknown | null;
+  size: number;
+  push: (data: unknown) => INode;
+  pop: () => unknown;
+}
+export class Stack implements IStack {
+  top: IStack['top'] = null;
+  size: IStack['size'] = 0;
+
+  public push(data: unknown) {
+    const node = new Node(data);
+
+    node.previous = this.top;
+    this.top = node;
+    this.size++;
+
+    return this.top as INode;
+  }
+
+  public pop() {
+    const temp = this.top as INode;
+    this.top = temp.previous;
+    this.size--;
+    return temp;
+  }
+}
+
+interface INode {
+  data: unknown;
+  previous: unknown | null;
+}
+export class Node implements INode {
+  data;
+  previous: INode['previous'];
+
+  constructor(data: unknown) {
+    this.data = data;
+    this.previous = null;
+  }
 }
