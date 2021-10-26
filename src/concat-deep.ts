62f899a1addbf9b144d192388e9d9635ad5e0015
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-10-25 23:47:44 -0300

    [feature] successfully implemented iterative array flat function

--- src/concat-deep.ts
+++ src/concat-deep.ts
@@ -1,21 +1,33 @@
+import { containsArray } from './contains-array';
 import { isArray } from './is-array';
 
 export const contactDeep = <TCollection extends unknown[]>(
   collection: TCollection,
 ) => {
-  const collectionLocal = [...collection];
-  const accumulator = [];
+  let stack: unknown[] = [...collection];
+  let stopConditionReached = false;
 
-  let count = 0;
-  while (count !== collectionLocal.length) {
-    if (isArray(collectionLocal[count])) {
-      accumulator.push(...(collectionLocal[count] as unknown[]));
-    } else {
-      accumulator.push(collectionLocal[count]);
+  while (stopConditionReached === false) {
+    const vectorLocal = [];
+
+    for (let i = 0; i < stack.length; i++) {
+      const element = stack[i];
+
+      if (isArray(element)) {
+        vectorLocal.push(...(element as unknown[]));
+      } else {
+        vectorLocal.push(element);
+      }
     }
 
-    count++;
+    stack = vectorLocal;
+
+    if (containsArray(stack)) {
+      stopConditionReached = false;
+    } else {
+      stopConditionReached = true;
+    }
   }
 
-  return accumulator;
+  return stack;
 };
