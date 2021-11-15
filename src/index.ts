b40d0861ef70aea19b598e469f8c3ad3aec421bd
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-11-14 23:43:01 -0300

    [feature] fully typed pipe function to enable fundamental functional programming principles

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import { concat } from './concat';
-import mergeDict from './deep-merge';
-import { protoFActory } from './factory-function';
-import { flatten } from './flatten';
-import { LinkedList } from './linked-list';
-const arr = [1, 2, [3, 4, [5, 6, [7, [8, 9, 10]]]]];
-const concatme = concat([[1], [2], [3, 4], [5, 6, 1, [3, 4]]]);
+import { pipe } from './pipe';
 
-// console.log(flatten(arr));
+const add1 = (x: number) => x + 1;
+const add3 = (x: number) => x + 3;
 
-const list = new LinkedList();
+const add4 = pipe(add1, add3);
 
-// list.append('hello');
-// list.append('Ok');
-// list.append('bye');
-// list.append('bye bye');
-
-// list.insert(1, 'I replace OK');
-
-// list.print();
-
-console.log(mergeDict({ abc: { def: { ghi: '123' } } }, {}));
+console.log(add4(5));
