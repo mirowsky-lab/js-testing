11bf9f2dc64bf543222c6bf75b685bc5e70fe5ad
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-11-23 23:55:04 -0300

    [refactor] better test function to go along with assertion functions from earlier

--- src/lessons/module-1-fundamentals-of-testing/simple.ts
+++ src/lessons/module-1-fundamentals-of-testing/simple.ts
@@ -1,34 +1,40 @@
 const sum = (a: number, b: number) => a + b;
-const buggedSum = (a: number, b: number) => a - b
-
-
 const subtract = (a: number, b: number) => a - b;
 
-let result = sum(3, 7);
-let faillingResult = buggedSum(3, 7)
-
-let expected = 10;
-expect(result).toBe(expected)
-expect(faillingResult).toBe(expected)
-
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
-
-
-function expect<TActual , TExpected extends TActual >(actual: TActual) {
-    return {
-        toBe(expected: TExpected) {
-            if (actual !== expected) {
-                throw new Error(`${actual} is not equal to ${expected}`)
-            }
-        },
-        toEqual(expected: TExpected) {
-        },
-        toBeGreaterThan(expected: TExpected) {
-
-        }
-    }
+function sumTest() {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
 }
 
- 
+test('sum adds numbers', sumTest);
+
+function substractTest() {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+}
+
+test('subtract numbers', substractTest);
+
+function test(title: string, callback: (...args: unknown[]) => void): void {
+  try {
+    callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.error(`❌ ${title}`);
+    console.error(error);
+  }
+}
+
+function expect<TActual, TExpected extends TActual>(actual: TActual) {
+  return {
+    toBe(expected: TExpected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected: TExpected) {},
+    toBeGreaterThan(expected: TExpected) {},
+  };
+}
